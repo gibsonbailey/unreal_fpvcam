4b3b743f3e8838967c7653131b4c225cc2bc8f4a
gibsonbailey <baileytrefts@gmail.com>
2024-12-08 19:17:53 -0800

    Add new baseclass for VRPawn blueprint.

---
+++ Source/MyBlankVRProject/MyVRPawn.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyVRPawn.h"
+
+// Sets default values
+AMyVRPawn::AMyVRPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+// void AMyVRPawn::BeginPlay()
+// {
+// 	Super::BeginPlay();
+	
+// }
+
+// // Called every frame
+// void AMyVRPawn::Tick(float DeltaTime)
+// {
+// 	Super::Tick(DeltaTime);
+
+// }
+
+// // Called to bind functionality to input
+// void AMyVRPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+// {
+// 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+// }
+
